a6ce29d02db5ff7b65adc1814fb0075c5b580884
Git-Faz <fahad.blr@icloud.com>
2026-03-11 23:19:22 +0530

    Added indexing

--- backend/src/main/java/com/faz/ecommerce/entity/Product.java
+++ backend/src/main/java/com/faz/ecommerce/entity/Product.java
@@ -10,7 +10,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name="products")
+@Table(
+    name="products",
+    indexes = {
+        @Index (name="idx_product_name", columnList = "name"),
+        @Index(name = "idx_product_price", columnList = "price"),
+        @Index(name = "idx_product_category", columnList = "category")
+    }
+)
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
