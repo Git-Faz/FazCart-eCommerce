180c45c0b7172d9f00b2ffa9f1154c4e5c510a6e
Git-Faz <fahad.blr@icloud.com>
2026-04-04 23:03:04 +0530

    refactired hook

--- frontend/src/app/hooks.ts
+++ frontend/src/app/hooks.ts
@@ -1,5 +1,22 @@
 import {useDispatch, useSelector} from 'react-redux' ;
 import type { RootState, AppDispatch } from './store';
+import { useState, useEffect } from 'react';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+export default function useIsDesktop() {
+  const [isDesktop, setIsDesktop] = useState(
+    window.matchMedia("(min-width: 768px)").matches
+  );
+
+  useEffect(() => {
+    const media = window.matchMedia("(min-width: 768px)");
+    const listener = () => setIsDesktop(media.matches);
+
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
+  }, []);
+
+  return isDesktop;
+}
