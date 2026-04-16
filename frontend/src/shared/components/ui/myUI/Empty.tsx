51435118ebae891eaa26fcf31390de6d205665af
Fahad Sheikh <faz@Fahads-MacBook-Pro.local>
2026-04-16 22:33:31 +0530

    minor style changes

--- frontend/src/shared/components/ui/myUI/Empty.tsx
+++ frontend/src/shared/components/ui/myUI/Empty.tsx
@@ -3,15 +3,16 @@ import { cn } from "@/shared/utils/utils";
 import { motion } from "framer-motion";
 
 interface EmptyStateProps {
-  message: string, desClassName?: string
+  message: string;
+  desClassName?: string;
 }
 
 const container = {
   animate: {
     transition: {
-      staggerChildren: 0.08
-    }
-  }
+      staggerChildren: 0.08,
+    },
+  },
 };
 
 const letter = {
@@ -21,21 +22,21 @@ const letter = {
     transition: {
       duration: 0.6,
       repeat: Infinity,
-      repeatDelay: 1
-    }
-  }
+      repeatDelay: 1,
+    },
+  },
 };
 
-export default function Empty({message, desClassName}: EmptyStateProps): JSX.Element {
+export default function Empty({
+  message,
+  desClassName,
+}: EmptyStateProps): JSX.Element {
+  const text = "EMPTY".split("");
 
-    const text = "EMPTY".split("");
-
-    //const bg = theme === "dark" ? darkBg : lightBg;
+  //const bg = theme === "dark" ? darkBg : lightBg;
 
   return (
-    <div
-      className="m-auto flex h-full w-full shrink flex-col bg-contain bg-no-repeat"
-    >
+    <div className="m-auto flex h-full w-full shrink flex-col bg-contain bg-no-repeat">
       <motion.h1
         variants={container}
         initial="initial"
@@ -43,16 +44,19 @@ export default function Empty({message, desClassName}: EmptyStateProps): JSX.Ele
         className="errorTitle mx-auto w-full font-[retrofloral] text-[58px] sm:text-[70px] md:text-[100px] lg:text-[150px] text-center dark:text-blue-500/50 text-amber-300"
       >
         {text.map((char, index) => (
-          <motion.span
-            key={index}
-            variants={letter}
-            className="inline-block"
-          >
+          <motion.span key={index} variants={letter} className="inline-block">
             {char}
           </motion.span>
         ))}
       </motion.h1>
-      <p className={cn("text-center font-bold text-2xl sm:text-2xl md:text-[30px] lg:text-[50px] dark:text-blue-500/50 text-amber-300", desClassName)}>{message}</p>
+      <p
+        className={cn(
+          "text-center font-bold text-2xl sm:text-2xl md:text-[30px] lg:text-[50px] dark:text-blue-500/50 text-amber-300",
+          desClassName,
+        )}
+      >
+        {message}
+      </p>
     </div>
-  )
+  );
 }
